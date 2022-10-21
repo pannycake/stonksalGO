b665f5f5afbdcdacb8df3f71bfc3e1289cca5aec
pannycake <114693130+pannycake@users.noreply.github.com>
2022-10-20 21:25:09 -0400

    Update stonks.go

--- stonks.go
+++ stonks.go
@@ -1,23 +1,69 @@
 package main
 
 import (
-	"fmt"
-	"math/rand"
-	"time"
+    "fmt"
+    "math/rand"
+    "sort"
+    "sync"
+    "time"
 )
 
+const MAX_CONCURRENCY int = 365
+
+func getStonks() {
+
+}
+
 func main() {
+    min := 93
+    max := 100
 
-	today := time.Now()
-	tomorrow := today.AddDate(0, 0, 1)
+    answers := []string{
+        "green",
+        "red",
+    }
 
-	min := 93
-	max := 100
-	answers := []string{
-		"green",
-		"red",
-	}
-	rand.Seed(time.Now().UTC().UnixNano())
-	fmt.Println("Tomorrow:", tomorrow.Month(), tomorrow.Day(), tomorrow.Year(), "will be a", answers[rand.Intn(len(answers))], "day")
-	fmt.Println("Accuracy: ", rand.Intn(max-min)+min, "%")
+    var wg sync.WaitGroup
+
+    date := time.Now()
+
+    results := []string{}
+    resultsChan := make(chan string, MAX_CONCURRENCY)
+
+    for i := 0; i < MAX_CONCURRENCY; i++ {
+        wg.Add(1)
+
+        go func(iteration int) {
+            defer func() {
+                wg.Done()
+
+            }()
+            rand.Seed(time.Now().UTC().UnixNano())
+
+            result := fmt.Sprintf("%v will be a %v day (Accuracy: %v%%)",
+                date.AddDate(0, 0, iteration).Format("2006-01-02"),
+                answers[rand.Intn(len(answers))],
+                rand.Intn(max-min)+min,
+            )
+
+            resultsChan <- result
+        }(i)
+    }
+
+    go func() {
+        for j := range resultsChan {
+            results = append(results, j)
+        }
+    }()
+
+    wg.Wait()
+
+    sort.Slice(results, func(i, j int) bool {
+        return results[i] < results[j]
+    })
+
+    for _, i := range results {
+        fmt.Println(i)
+    }
+
 }
